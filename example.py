35a007a355be09d7e1ca1d835400f5614e512959
Andrey Cherny <andrey.cherny@latoken.com>
2021-10-22 14:18:09 +0300

    first test

---
+++ example.py
@@ -0,0 +1,13 @@
+
+
+def add(a, b):
+    return a + b
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+
+
+def substruct(a, b):
+    return a - b
---
+++ test_example.py
@@ -0,0 +1,4 @@
+from example import substruct
+
+def test_subtract():
+    assert substruct(2, 3) == -1
